66d8d408b8049a7c4ff9a75eed8084ec9b269f59
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-10-28 21:34:05 +0100

    clear repo for clear django version starup

--- create_tables.py
+++
@@ -1,7 +0,0 @@
-from database import Base, engine
-from models import User, Game, Role, UserInGame
-
-# This will create all tables that inherit from Base
-Base.metadata.create_all(bind=engine)
-
-print("Tables created!")
